eea559d0912a1b8f802f006ca413f7d4b04f2d74
unknown <megautam-git>
2018-08-31 12:09:05 +0530

    method added

--- One.java
+++ One.java
@@ -1,5 +1,28 @@
+import java.util.*;
+
 public class One{
     public static void main(String[] args) {
         System.out.println("gautam in java");
+        Scanner sc=new Scanner(System.in);
+        int[] a=new int[5];
+        int[] b=new int[5];
+        for (int i = 0; i < a.length; i++) {
+            a[i]=sc.nextInt();
+        }
+        for (int i = 0; i < b.length; i++) {
+            b[i]=sc.nextInt();
+        }
+        int[] c=doSum(a,b);
+        for (int i = 0; i < c.length; i++) {
+            System.out.print(c[i]);
+        }
     }   
+    public static int[] doSum(int[] a,int[] b){
+        int[] c=new int[5];
+       for (int i = 0; i < c.length; i++) {
+           c[i]=a[i]+b[i];
+       }
+       return c;
+    }
+    
    }
